786a5578fb590d0d1f956bea1f01ad8ac84170f7
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-05-02 19:34:13 +0900

    commit from intellij

--- src/main/java/com/example/quizapp/QuizAppController.java
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -1,12 +1,24 @@
 package com.example.quizapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class QuizAppController {
+    private List<String> quizzes = new ArrayList<>();
     @GetMapping("/show")
     public String show() {
-        return "Hello, World";
+        return quizzes.toString();
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestParam String question, @RequestParam boolean answer) {
+        String quiz = question + ":" + answer;
+        quizzes.add(quiz);
     }
 }
