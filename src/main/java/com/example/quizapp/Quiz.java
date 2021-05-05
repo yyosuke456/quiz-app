3e7f34db7a5e5fe95e0db322b973cd224cfb5a92
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-05-05 23:12:12 +0900

    fromStringメソッド、テストクラスを追加

--- src/main/java/com/example/quizapp/Quiz.java
+++ src/main/java/com/example/quizapp/Quiz.java
@@ -1,5 +1,6 @@
 package com.example.quizapp;
 
+// すべてのJavaクラスはObjectを親クラスに持つ。なので、toString()とか使える
 public class Quiz {
     /**
      * 問題文
@@ -21,8 +22,17 @@ public class Quiz {
         return answer;
     }
 
+    @Override
     public String toString() {
         String marubatsu = answer ? "○" : "✕";
         return question + " " + marubatsu;
+    }
+
+    // line: 問題文○
+    public static Quiz fromString(String line){
+        String question = line.substring(0, line.length() - 2);
+        boolean answer = line.endsWith("○");
+
+        return new Quiz(question, answer);
     }
 }
--- src/test/java/com/example/quizapp/QuizTest.java
+++ src/test/java/com/example/quizapp/QuizTest.java
@@ -15,4 +15,18 @@ class QuizTest {
         Quiz quiz = new Quiz("問題文", false);
         assertEquals(quiz.toString(), "問題文 ✕");
     }
+    @Test
+    public void fromStringマルのとき() {
+        String line = "問題文1 ○";
+        Quiz quiz = Quiz.fromString(line);
+        assertEquals(quiz.getQuestion(), "問題文1");
+        assertEquals(quiz.getAnswer(), true);
+    }
+    @Test
+    public void fromStringバツのとき() {
+        String line = "問題文1 ✕";
+        Quiz quiz = Quiz.fromString(line);
+        assertEquals(quiz.getQuestion(), "問題文1");
+        assertEquals(quiz.getAnswer(), false);
+    }
 }
