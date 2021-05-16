6a74668490462dc3c5701ab15583ce92c0464baf
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-05-16 20:01:39 +0900

    クイズの正解チェックを実装

--- src/main/java/com/example/quizapp/QuizAppController.java
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -40,24 +40,23 @@ public class QuizAppController {
     }
 
     @GetMapping("/check")
-    public String check(@RequestParam String question, @RequestParam boolean answer) {
+    public String check(Model model, @RequestParam String question, @RequestParam boolean answer) {
         // 回答が正しいかをチェックして結果を返す。
         // questionを登録済みのクイズから検索
         for(Quiz quiz: quizzes) {
             // クイズが見つかったらanswerを取得する
             if (quiz.getQuestion().equals(question)) {
-                // 登録されているanswerと登録されているanswerを比較し、一致していれば「正解」
+                model.addAttribute("quiz", quiz);
+                // 登録されているanswerと登録されているanswerを比較し、一致していれば「正解」をセット
                 if(quiz.getAnswer() == answer) {
-                    return "正解！";
-                }else{// 一致していなければ「不正解」
-                    return "不正解！";
+                    model.addAttribute("result", "正解！");
+                }else{// 一致していなければ「不正解」をセット
+                    model.addAttribute("result", "不正解！");
                 }
-            } else {
-                return "問題がありません";
             }
         }
         // 見つからなかったら「問題がありません」
-        return "問題がありません";
+        return "answer";
     }
 
     @PostMapping("/save")
